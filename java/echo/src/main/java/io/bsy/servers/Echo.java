5f9d1b52258f40990a061aa1e30b2f5e65dabed5
bsy <sergey.y.badanin@gmail.com>
2023-10-03 20:51:05 +0200

    Echo server in Java

---
+++ java/echo/src/main/java/io/bsy/servers/Echo.java
@@ -0,0 +1,53 @@
+package io.bsy.servers;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.logging.Logger;
+
+/*
+ * https://jenkov.com/tutorials/java-nio/selectors.html
+ * https://docs.oracle.com/javase/8/docs/api/java/nio/package-summary.html
+ * 
+ * https://www.happycoders.eu/java/java-19-features/#New_Methods_to_Create_Preallocated_HashMaps
+ * https://howtodoinjava.com/java/java-21-new-features/
+ */
+
+public class Echo {
+
+    private final static Logger logger = Logger.getLogger(Echo.class.getName());
+
+    public static void main(String[] args) throws IOException {
+
+        try (var serverSocketChannel = ServerSocketChannel.open();
+                var selector = Selector.open()) {
+            serverSocketChannel.bind(new InetSocketAddress(8091));
+
+            for (;;) {
+                var channel = serverSocketChannel.accept();
+
+                var buf = ByteBuffer.allocate(1024);
+                channel.configureBlocking(false);
+                var key = channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                key.attach(buf);
+
+                selector.select();
+                var keys = selector.selectedKeys();
+
+                for (var k : keys) {
+                    if (k.isWritable()) {
+                        var bf = (ByteBuffer)k.attachment();
+                        bf.hasRemaining();
+                    }
+                    
+                }
+
+            }
+        } catch (Exception e) {
+            logger.warning(String.format("Exception: %s", e));
+        }
+    }
+}
